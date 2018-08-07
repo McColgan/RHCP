9cd2eb4f6386e540aa17ce55c6e97b90b1c86bbb
Liam McColgan <l.mccolgan.connect@gmail.com>
2018-08-07 01:01:14 +0100

    07/08/28 commit: added responsiveness for current sections

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 $(document).ready(function(){
-    // Nav-Icon Animation
+    // Nav-Icon animation
     $('#nav_icon').click(function(){
         $('.nav_container').toggleClass('nav_container--open', 300);
         $(this).toggleClass('open');
     });
 
-    //Hiding header on scroll
+    // Hiding header on scroll
     let prevScrollPos = window.pageYOffset;
+    // Checking to see if screen width matches media query for desktop header
     let screenWidth = window.matchMedia( "(min-width: 800px)" );
     
     if (screenWidth.matches){
